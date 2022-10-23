07630b78f0c38217c2e960a3dd796e30d86a1b53
SongJwans <yhh5916@naver.com>
2022-10-23 21:43:18 +0900

    최초 커밋

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[]args){
+        int[] a ={ 4, 8, 2, 6, 10, 3, 1, 7, 9, 5 };
+        sort.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
